750985c3e70664c23080ba62c1e506d5a042dcb1
kali <qwertyuiopasdfghjkl12345678901234567890zxcvbnm@outlook.com>
2025-01-29 21:58:38 +0800

    添加注册物品方法并注册kunkun物品

--- src/main/java/name/xiaoheizi/ikunmod/IkunMod.java
+++ src/main/java/name/xiaoheizi/ikunmod/IkunMod.java
@@ -1,5 +1,6 @@
 package name.xiaoheizi.ikunmod;
 
+import name.xiaoheizi.ikunmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,6 +19,8 @@ public class IkunMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		ModItems.RegisterModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
--- src/main/java/name/xiaoheizi/ikunmod/item/ModItems.java
+++ src/main/java/name/xiaoheizi/ikunmod/item/ModItems.java
@@ -1,5 +1,6 @@
 package name.xiaoheizi.ikunmod.item;
 
+import name.xiaoheizi.ikunmod.IkunMod;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -7,7 +8,14 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item KUNKUN = registerItems("kunkun", new Item(new Item.Settings()));
+
     private static Item registerItems(String id, Item item) {
-        return (Item) Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.ofVanilla(id)), item);
+//        return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(IkunMod.MOD_ID, id)), item);
+        return Registry.register(Registries.ITEM, Identifier.of(IkunMod.MOD_ID, id), item);
+    }
+
+    public static void RegisterModItems() {
+        IkunMod.LOGGER.info("Registering Mod Items for " + IkunMod.MOD_ID);
     }
 }
